76ebd9f1c472a91ffcb7fef14f3cfe4ecccac125
Gaurav Tiwari <gauravtiwari01@gmail.com>
2024-11-11 23:39:52 -0500

    Large File Processing

---
+++ file_analysis/unique_words.py
@@ -0,0 +1,11 @@
+"""
+File with a function that reads a large text file and counts the number of unique words, 
+ignoring case and punctuation
+
+About dataset - Electrical Vehicle Population Data with columns 
+VIN (1-10),County,City,State,Postal Code,Model Year,Make,Model,Electric Vehicle Type,
+Clean Alternative Fuel Vehicle (CAFV) Eligibility,Electric Range,Base MSRP,Legislative District,
+DOL Vehicle ID,Vehicle Location,Electric Utility,2020 Census Tract
+"""
+
+
